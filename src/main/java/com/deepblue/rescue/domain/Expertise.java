ddfd4917446c81bca34a7876b62296c87760f3f1
Jairo <jairozapateirolopez@gmail.com>
2026-09-14 19:03:53 -0500

    feat: entidad Expertise

--- src/main/java/com/deepblue/rescue/domain/Expertise.java
+++ src/main/java/com/deepblue/rescue/domain/Expertise.java
@@ -1,4 +1,40 @@
 package com.deepblue.rescue.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "expertise")
 public class Expertise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name",unique = true,nullable = false,length = 150)
+    private String name;
+
+    public Expertise() {
+    }
+
+    public Expertise(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
+
